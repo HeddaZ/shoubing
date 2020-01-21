4a2168c624f02c3faf3ab8609f8ec47a173866d5
Hedda Zhang <heddaz@live.com>
2020-01-21 14:34:18 +0800

    New layout of Home.

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
-function showMessage(message) {
+function showMessage(message, warning) {
+    const infoBox = 'alert-success';
+    const infoIcon = 'glyphicon-info-sign';
+    const warningBox = 'alert-warning';
+    const warningIcon = 'glyphicon-warning-sign';
+
     let messageBox = $('#messageBox');
-    messageBox.text(message);
-    messageBox.parent().stop(null, true, true)
+    if (warning) {
+        messageBox.removeClass(infoBox).addClass(warningBox);
+        messageBox.find('.message-icon').removeClass(infoIcon).addClass(warningIcon);
+    }
+    else {
+        messageBox.removeClass(warningBox).addClass(infoBox);
+        messageBox.find('.message-icon').removeClass(warningIcon).addClass(infoIcon);
+    }
+    messageBox.find('.message-text').html(message);
+    messageBox.stop(null, true, true)
         .slideDown()
-        .delay(5000)
-        .slideUp();
+        .delay(6000)
+        .slideUp('fast');
+}
+
+function openWindow(url, width, height, name) {
+    if (!name) {
+        name = '_self';
+    }
+    let widthFeature = '', heightFeature = '';
+    if (width && !isNaN(width)) {
+        widthFeature = 'width=' + parseInt(width);
+    }
+    if (height && !isNaN(height)) {
+        heightFeature = 'height=' + parseInt(height);
+    }
+    window.open(url, name, widthFeature + heightFeature + 'menubar=no,toolbar=no,location=yes,status=no,scrollbars=yes,dependent=no');
+}
+
+function notChrome() {
+    return navigator && navigator.userAgent && navigator.userAgent.indexOf('Chrome') < 0;
 }
 
 $(function () {
-    $('.url-text').css('cursor', 'pointer')
+    if (notChrome()) {
+        showMessage('注意：本应用程序仅支持 Chrome 浏览器或 OBS 浏览器组件使用。', true);
+    }
+
+    $('.url-text')
         .each(function () {
             $(this).val($(this).data('url'));
         })
-        .on('paste', function () {
+        .blur(function () {
+            $(this).val($(this).data('url'));
+        })
+        .on('copy', function (e) {
+            showMessage(e.target.value + ' 已放入剪贴板！<br/>请将此地址填入 OBS 浏览器来源的 URL 中，并设置宽度 750 高度 630。');
+            return true;
+        })
+        .on('paste cut', function () {
             return false;
         })
-        .keydown(function () {
+        .keydown(function (e) {
+            if (e.ctrlKey || (e.ctrlKey && e.which == 67)) { // Ctrl+C
+                return true;
+            }
             return false;
         })
         .mouseenter(function () {
@@ -29,27 +74,22 @@ $(function () {
         .focus(function () {
             $(this).select();
             return false;
-        })
-        .blur(function () {
-            $(this).val($(this).data('url'));
         });
 
-    // 剪贴板
-    let clipboard = new ClipboardJS('.url-text', {
-        text: function (trigger) {
-            return trigger.getAttribute('data-url');
-        }
-    });
-    clipboard.on('success', function (e) {
-        showMessage('已复制 ' + e.text + ' 到剪贴板！请将此地址放入 OBS 浏览器来源 URL 中，或用 Chrome 打开进行手柄调试。')
-        e.clearSelection();
-    });
-    clipboard.on('error', function (e) {
-        showMessage('复制失败！请检查浏览器授权，或直接使用 Ctrl+C 复制。')
+    $('#chromeButton').click(function () {
+        openWindow($(this).data('url'));
+        return false;
     });
 
-    $('#chromeButton').click(function () {
-        window.open($(this).data('url'));
+    $('.btn-test').click(function () {
+        let targetId = $(this).data('target');
+        openWindow($(targetId).data('url'), 770, 650, targetId);
         return false;
+    });
+
+    // 剪贴板
+    let clipboard = new ClipboardJS('.btn-copy');
+    clipboard.on('error', function (e) {
+        showMessage('复制失败！<br/>请检查浏览器授权，或直接使用 Ctrl+C 复制。', true);
     });
 });
